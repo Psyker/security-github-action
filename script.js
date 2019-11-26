1d1be49780ce077d244f6391c03281629d6713af
Psyker <bourgoin.theo1@gmail.com>
2019-11-26 17:52:05 +0100

    Testing new approaches

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const Octokit = require("@octokit/rest");
+const fs = require('fs');
 
 const octokit = new Octokit({
     'auth': 'secret',
     'userAgent': 'security-js script'
 });
+fs.readFile('./test.json', (err, data) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(data)
+});
 
-console.log('hello world');
+fs.unlink('./test.json', (err) => {
+    if (err) throw err;
+    console.log('test.json was deleted');
+});
